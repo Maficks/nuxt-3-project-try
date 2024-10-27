6830827a058c1d38dbd5cf92168cbfefdc26b303
Maficks <berrrserk2020@mail.ru>
2024-10-27 21:57:41 +0500

    nuxt 3

---
+++ app.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="centreDiv">
+      <h1>КРАСИВЫЙ ТЕКСТ))</h1>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-family: "Montserrat", sans-serif;
+}
+.centreDiv {
+  justify-content: center;
+  display: flex;
+}
+</style>
+<script setup lang="ts"></script>
